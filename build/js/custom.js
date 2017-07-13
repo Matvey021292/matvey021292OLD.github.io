c90bf94703eda3ea190e1595e1849b49c9f10b0c
Matvey <raul021292@gmail.com>
2017-07-13 16:31:16 +0300

    al section

--- build/js/custom.js
+++ build/js/custom.js
@@ -112,6 +112,6 @@ window.onload = function() {
   // INJECT CSS
   var css = document.createElement("style");
   css.type = "text/css";
-  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666;color:#18183a;font-family:'Intro Inline', sans-serif;font-weight:100; }";
+  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666;color:#18183a;font-family:'Intro-Inline', sans-serif;font-weight:100; }";
   document.body.appendChild(css);
 };
