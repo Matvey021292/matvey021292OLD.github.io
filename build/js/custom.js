8afac292ec7f909199bbb1b4c39a5741c8da7eff
Matvey <raul021292@gmail.com>
2017-07-14 17:08:53 +0300

    call

--- build/js/custom.js
+++ build/js/custom.js
@@ -112,7 +112,7 @@ window.onload = function() {
   // INJECT CSS
   var css = document.createElement("style");
   css.type = "text/css";
-  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666;color:#18183a;font-family:'Intro-Inline', sans-serif;font-weight:100; }";
+  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666;color:#18183a;font-family:'Intro-Inline', sans-serif;font-weight:100;text-shadow: 6px 6px 0px rgba(0,0,0,0.2); }";
   document.body.appendChild(css);
 };
 
